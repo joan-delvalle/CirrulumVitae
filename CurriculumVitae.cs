b2c5a448af6c696d01f5b984487c1b165428556f
joan-delvalle <joandelvalle865@gmail.com>
2025-11-28 19:13:08 +0800

    CurriculumVitae

---
+++ CurriculumVitae.cs
@@ -0,0 +1,22 @@
+namespace CurriculumVitae.Models
+{
+    public class CurriculumVitae
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public string DateofBirth{ get; set; }
+        public string PlaceofBirth { get; set; }
+        public string Gender { get; set; }
+        public string MartialStatus { get; set; }
+        public string JobTitle { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+        public string Skills { get; set; }
+        public string Interest { get; set; }
+        public string Refrences { get; set; }
+
+    }
+}
